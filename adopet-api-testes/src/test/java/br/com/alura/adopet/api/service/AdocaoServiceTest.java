cb9a66732b3f84f10ec371c1e52d05c78d0ad528
murilochag <murilochagas10@outlook.com>
2024-06-01 21:10:14 -0300

    finalização do terceiro curso = Boas práticas de programação: automatizando testes com Java (sem o desafio)

---
+++ adopet-api-testes/src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -0,0 +1,108 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Adocao;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.Tutor;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import br.com.alura.adopet.api.repository.PetRepository;
+import br.com.alura.adopet.api.repository.TutorRepository;
+import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class AdocaoServiceTest {
+
+    @InjectMocks
+    private AdocaoService adocaoService;
+
+    @Mock
+    private AdocaoRepository repository;
+
+    @Mock
+    private PetRepository petRepository;
+
+    @Mock
+    private TutorRepository tutorRepository;
+
+    @Mock
+    private EmailService emailService;
+
+    @Spy
+    private List<ValidacaoSolicitacaoAdocao> validacoes = new ArrayList<>();
+
+    @Mock
+    private ValidacaoSolicitacaoAdocao validador1;
+
+    @Mock
+    private ValidacaoSolicitacaoAdocao validador2;
+
+    @Mock
+    private Pet pet;
+
+    @Mock
+    private Tutor tutor;
+
+    @Mock
+    private Abrigo abrigo;
+
+    private SolicitacaoAdocaoDto dto;
+
+    @Captor
+    private ArgumentCaptor<Adocao> adocaoCaptor;
+
+
+    @Test
+    void deveSalvarAdocaoAoSolicitar() {
+
+        //ARRANGE
+        this.dto = new SolicitacaoAdocaoDto(10l, 20l, "motivo qualquer");
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
+        BDDMockito.given(pet.getAbrigo()).willReturn(abrigo);
+
+        //ACT
+        adocaoService.solicitar(dto);
+
+        //ASSERT
+        BDDMockito.then(repository).should().save(adocaoCaptor.capture()); // pede para o mokito capturar o objeto que foi chamado
+        Adocao adocaoSalva = adocaoCaptor.getValue();
+
+        assertEquals(pet,adocaoSalva.getPet());
+        assertEquals(tutor,adocaoSalva.getTutor());
+        assertEquals(dto.motivo(),adocaoSalva.getMotivo());
+
+    }
+
+    @Test
+    void deveChamarValidadoresDeAdocaoAoSolicitar() {
+
+        //ARRANGE
+        this.dto = new SolicitacaoAdocaoDto(10l, 20l, "motivo qualquer");
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
+        BDDMockito.given(pet.getAbrigo()).willReturn(abrigo);
+
+        validacoes.add(validador1);
+        validacoes.add(validador2);
+
+        //ACT
+        adocaoService.solicitar(dto);
+
+        //ASSERT
+        BDDMockito.then(validador1).should().validar(dto);
+        BDDMockito.then(validador2).should().validar(dto);
+
+
+    }
+
+}
---
+++ adopet-api-testes/src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
@@ -0,0 +1,56 @@
+package br.com.alura.adopet.api.validacoes;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ValidacaoPetDisponivelTest {
+
+    @InjectMocks
+    private ValidacaoPetDisponivel validacaoPetDisponivel;
+
+    @Mock
+    private PetRepository petRepository;
+
+    @Mock
+    private Pet pet;
+
+    @Mock
+    private SolicitacaoAdocaoDto dto;
+
+    @Test
+    void deveriaPermitirSolicitacaoDeAdocaoPet() {
+
+        //ARRANGE
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(pet.getAdotado()).willReturn(false);
+
+        //ACT + ASSERTIONS
+        Assertions.assertDoesNotThrow(() -> validacaoPetDisponivel.validar(dto));
+    }
+
+    @Test
+    void naoDeveriaPermitirSolicitacaoDeAdocaoPet() {
+
+        //ARRANGE
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(pet.getAdotado()).willReturn(true);
+
+        //ACT + ASSERTIONS
+        Assertions.assertThrows(ValidacaoException.class, () -> validacaoPetDisponivel.validar(dto));
+    }
+
+
+
+}
